bfbed9ae2e31e3af1b202599ecc59931ffab33d5
AlexandrTurov <alexandr.turov@mail.ru>
2024-10-14 14:29:16 +0700

    add env settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,26 @@
-import * as path from 'path';
+import * as path from "path";
+import * as process from "process";
 
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
-import { FileModule } from './file/file.module';
-import { TrackModule } from './track/track.module';
+import { FileModule } from "./file/file.module";
+import { TrackModule } from "./track/track.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/music'),
+    MongooseModule.forRoot(process.env.DB_HOST),
     TrackModule,
     FileModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
